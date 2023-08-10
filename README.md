f2086b0a1555814dbfb3a18f5f3226657a1a88d3
CheaterScript <iluozan@126.com>
2023-08-10 18:54:59 +0800

    feat: update project

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # docker-monitoring
 
 # set-ExecutionPolicy RemoteSigned
+
+firewall-cmd --zone=public --add-port=9100/tcp --permanent 
+
+curl -X PUT -d '{"id": "Linux-1","name": "Linux","address": "192.168.225.1","port": 9182,"tags": ["service"],"checks": [{"http": "http://192.168.225.1:9182","interval": "5s"}]}' http://192.168.225.128:8500/v1/agent/service/register
+
+netsh advfirewall firewall add rule name=9100 protocol=TCP localport=9100 dir=in action=allow
+
+curl -X PUT http://192.168.225.128:8500/v1/agent/service/deregister/node_exporter01
+
+19269
--- exporter/windows/install_node.bat
+++ exporter/windows/install_node.bat
@@ -4,10 +4,10 @@ pushd %~dp0
 
 IF "%PROCESSOR_ARCHITECTURE%" == "X86" (
     msiexec /x %~dp0x86\windows_exporter-0.23.1-386.msi /q
-    msiexec /i %~dp0x86\windows_exporter-0.23.1-386.msi ENABLED_COLLECTORS="ad,iis,logon,memory,process,tcp,thermalzone" /q
+    msiexec /i %~dp0x86\windows_exporter-0.23.1-386.msi ENABLED_COLLECTORS="ad,adfs,cache,cpu,cpu_info,cs,container,dfsr,dhcp,dns,fsrmquota,iis,logical_disk,logon,memory,msmq,mssql,netframework_clrexceptions,netframework_clrinterop,netframework_clrjit,netframework_clrloading,netframework_clrlocksandthreads,netframework_clrmemory,netframework_clrremoting,netframework_clrsecurity,net,os,process,remote_fx,service,tcp,time,vmware" TEXTFILE_DIR="C:\custom_metrics" /q
 ) ELSE (
     msiexec /x %~dp0x64\windows_exporter-0.23.1-amd64.msi /q
-    msiexec /i %~dp0x64\windows_exporter-0.23.1-amd64.msi ENABLED_COLLECTORS="ad,iis,logon,memory,process,tcp,thermalzone" /q
+    msiexec /i %~dp0x64\windows_exporter-0.23.1-amd64.msi ENABLED_COLLECTORS="ad,adfs,cache,cpu,cpu_info,cs,container,dfsr,dhcp,dns,fsrmquota,iis,logical_disk,logon,memory,msmq,mssql,netframework_clrexceptions,netframework_clrinterop,netframework_clrjit,netframework_clrloading,netframework_clrlocksandthreads,netframework_clrmemory,netframework_clrremoting,netframework_clrsecurity,net,os,process,remote_fx,service,tcp,time,vmware" TEXTFILE_DIR="C:\custom_metrics" /q
 )
 
 @popd
